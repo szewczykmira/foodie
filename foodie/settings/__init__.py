b19586def201f868d60fcc860ae52aab0f16ff12
Mira <szewczyk.mira@gmail.com>
2017-10-20 18:08:12 +0200

    Update connection with database

--- foodie/settings/__init__.py
+++ foodie/settings/__init__.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'foodie.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'foodie',
+        'USER': 'foodie',
+        'PASSWORD': 'foodie',
+        'HOST': 'localhost',
+        'PORT': 9432,
     }
 }
 
