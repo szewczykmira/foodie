f17c287015a5c13064862a4b1d0909bcd0b0ef52
Mira <szewczyk.mira@gmail.com>
2017-10-20 20:32:50 +0200

    Add compiling uikit less

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,37 @@
 var path = require('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
-    entry: './static/source/js/main.js',
+    entry: ['./static/source/js/main.js', './static/source/less/main.less'],
     output: {
-        filename: 'scripts/main.js',
-        path: path.resolve(__dirname, 'static')
-    }
-};
+        filename: './static/dist/scripts/main.js',
+    },
+    module: {
+      rules: [
+
+        {
+        test: /\.less$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: true
+              }
+            },
+            {
+              loader: 'less-loader'
+            }
+          ]
+        })
+      },
+      ]
+    },
+    plugins: [
+      new ExtractTextPlugin({ // define where to save the file
+        filename: 'static/dist/css/[name].bundle.css',
+        allChunks: true,
+      }),
+    ]
+  };
