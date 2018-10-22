2a7a9b91afb3a119de756ddce08a17507e7ea62e
Mira <szewczyk.mira@gmail.com>
2018-10-22 20:30:54 +0200

    Use model serializer

--- fridge/serializers.py
+++ fridge/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 
 from .models import Food
 
-class FoodSerializer(serializers.Serializer):
+class FoodSerializer(serializers.ModelSerializer):
     class Meta:
         model = Food
         fields = ('since_when', 'expiration_date', 'name', 'quantity')
